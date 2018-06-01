62fd7b58f7ab35e7996313c3730666b01018ff93
cippero <gilwein@gmail.com>
2018-06-01 11:05:04 -0700

    cleanup

--- server.js
+++ server.js
@@ -3,51 +3,43 @@ require('dotenv').config(); //loads the .env
 var express        = require('express');
 var expressLayouts = require('express-ejs-layouts');
 var bodyParser     = require('body-parser');
-var flash          = require('connect-flash');
+// var flash          = require('connect-flash');
 var mongoose       = require('mongoose');
 var morgan         = require('morgan');
-var passport       = require('./config/passportConfig');
-var session        = require('express-session');
-var User           = require('./models/user');
+// var User           = require('./models/user');
 var app            = express();
 
 ///////////////CONNECT TO DATABASE////////////////
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nutrifood');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/travelBuddy');
 
 ////////////////SET & USE MODULES/////////////////
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(expressLayouts);
 app.use(morgan('tiny'));
-app.use(session({
-	secret:            process.env.SESSION_SECRET,
-	resave:            false,
-	//saveUninitialized: true
-	saveUninitialized: false
-}));
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.static('public'));
+// app.use(session({
+// 	secret:            process.env.SESSION_SECRET,
+// 	resave:            false,
+// 	//saveUninitialized: true
+// 	saveUninitialized: false
+// }));
+// app.use(flash());
+// app.use(passport.initialize());
+// app.use(passport.session());
+// app.use(express.static('public'));
 
-//just a convenience that makes life easier:
-app.use(function(req, res, next){
-	res.locals.currentUser = req.user;
-	res.locals.alerts = req.flash();
-	next();
-});
 
 /////////////////////ROUTES///////////////////////
 app.get('/', function(req,res) {
 	res.render('search', {currentPage: 'Search'});
 })
 
-app.use('/auth', require('./routes/auth'));
-app.use('/search', require('./routes/search'));
-app.use('/profile', require('./routes/profile'));
+// app.use('/auth', require('./routes/auth'));
+// app.use('/search', require('./routes/search'));
+// app.use('/profile', require('./routes/profile'));
 
 app.use(function(req, res){
-    res.status(404).render('404', {currentPage: 'Search'});
+    res.status(404).render('404');
 });
 
 ////////////////////LISTENING/////////////////////
