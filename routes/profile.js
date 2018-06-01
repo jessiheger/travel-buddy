e970ca80dc1c77f35b5794643ce57469f273dce7
cippero <gilwein@gmail.com>
2018-06-01 10:42:59 -0700

    project initialization

---
+++ routes/profile.js
@@ -0,0 +1,23 @@
+var express    = require('express');
+var request    = require('request');
+var router     = express.Router();
+var isLoggedIn = require('../middleware/isLoggedIn');
+var User       = require('../models/user');
+
+router.get('/', isLoggedIn, function(req, res) {
+	res.render("profile", {currentPage: 'Profile'});
+})
+
+router.get('/edit', isLoggedIn, function(req, res) {
+	res.render("editProfile", {currentPage: 'Edit Profile'});
+})
+
+router.put('/edit', isLoggedIn, function(req, res) {
+	//console.log('updating');
+	User.findOneAndUpdate({_id: res.locals.currentUser._id}, req.body, function(err, user) {
+		if (err) { return console.log("err:", err); }
+		res.render("profile", {currentPage: 'Profile'});
+	})
+})
+
+module.exports = router;
